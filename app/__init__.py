40c7ad8c58bd8bc83a66fb89e9ccffba19de118b
Your Name <ermolaeva@agent.ru>
2017-05-10 21:01:29 +1000

    init

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
 app.config['DEBUG'] = True
 
 from app.controllers import payload
 from app.errors import err_handlers
+
+app.run(host='0.0.0.0', port=port)
