fe10eedb58b486f8ca6d99c062bb3dcdc6c1345a
Your Name <ermolaeva@agent.ru>
2017-05-10 20:10:41 +1000

    added debug

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
+app.config['DEBUG'] = True
 
 from app.controllers import payload
 from app.errors import err_handlers
