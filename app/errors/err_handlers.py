c53408eab97b31e1d2cc2e456e1ebe3186814290
Your Name <ermolaeva@agent.ru>
2017-05-10 21:12:57 +1000

    jasonify response

--- app/errors/err_handlers.py
+++ app/errors/err_handlers.py
@@ -1,9 +1,10 @@
-from json import dumps
+from flask import jsonify
 
 from app import app
 
 
 @app.errorhandler(Exception)
 def all_exception_handler(error):
-    return dumps({"error": "Could not decode request: JSON parsing failed"}), 400
+
+    return jsonify({"error": "Could not decode request: JSON parsing failed"}), 400
 
