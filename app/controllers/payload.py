4d831e94ef58142966248cd7341afb8de5f34bf0
Your Name <ermolaeva@agent.ru>
2017-05-10 19:07:49 +1000

    added alive indicator

--- app/controllers/payload.py
+++ app/controllers/payload.py
@@ -4,6 +4,11 @@ from app import app
 from app.services.processor import process
 
 
+@app.route('/alive', methods=['GET'])
+def alive():
+    return 'alive'
+
+
 @app.route('/payloads', methods=['POST'])
 def process_payloads():
     return process(req.data)
