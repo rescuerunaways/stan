fc9d80d2f7fd304c0b8a2623a0beacac3af83ffd
Your Name <ermolaeva@agent.ru>
2017-05-10 19:27:17 +1000

    fixed port binding

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import os
+
 from flask import Flask
 
 
 app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
 from app.controllers import payload
 from app.errors import err_handlers
